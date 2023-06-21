3fc394752ea8da527c2e4087228cf45301675c43
Rahul Iyer <rahulair@gmail.com>
2023-06-21 02:32:11 -0700

    Separate indexing from qa

--- qa.py
+++ qa.py
@@ -1,28 +1,43 @@
 import sys
+import os
+import argparse
 
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+
 from langchain.chains import RetrievalQA
 
-from langchain.document_loaders import PyPDFLoader
+from langchain.vectorstores.faiss import FAISS
 
-loader = PyPDFLoader('./scaling_people.pdf')
+def qa(index_path):
+    vectorstore = None
+    if os.path.exists(index_path):
+        print("Index found. Loading index...")        
+        vectorstore = FAISS.load_local(index_path, OpenAIEmbeddings())
+    else:
+        print(f"Index {index_path} doesn't exist")
+        sys.exit(-1)
 
-documents = loader.load()
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=0)
-texts = text_splitter.split_documents(documents)
+    qa = RetrievalQA.from_chain_type(
+        llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k"), 
+        chain_type="stuff", 
+        retriever=vectorstore.as_retriever())
 
-embeddings = OpenAIEmbeddings()
-docsearch = Chroma.from_documents(texts, embeddings)
+    while True:
+        query = input("Ask a question: ")
+        result = qa.run(query)
+        print(f"{result}\n")
 
-qa = RetrievalQA.from_chain_type(
-    llm=OpenAI(temperature=0), 
-    chain_type="stuff", 
-    retriever=docsearch.as_retriever())
+if __name__ == "__main__":
+    # Create an ArgumentParser object
+    parser = argparse.ArgumentParser()
 
-while True:
-    query = input("Ask a question: ")
-    result = qa.run(query)
-    print(result)
+    # Add required string arguments
+    parser.add_argument('--idx_path', type=str, required=True, help='Index file name')
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+    idx_path = args.idx_path
+
+    qa(idx_path)
+
